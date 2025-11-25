5d447692be2fbc61d9ef293c91c9c4645956a86f
Micha Buchholz <mail@lean-stack.rocks>
2025-11-25 17:07:54 +0100

    chore: Create minimal Joomla component. Initial commit.

---
+++ administrator/components/com_cinetixx/src/Controller/DisplayController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Weltspiegel\Component\Cinetixx\Administrator\Controller;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+
+/**
+ * Cinetixx master display controller.
+ *
+ * @since  1.0.0
+ */
+class DisplayController extends BaseController
+{
+
+	/**
+	 * The default view
+	 *
+	 * @var string
+	 * @since version 1.0.0
+	 */
+	protected $default_view = 'events';
+}
---
+++ administrator/components/com_cinetixx/src/View/Events/HtmlView.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Weltspiegel\Component\Cinetixx\Administrator\View\Events;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+
+/**
+ * View class for the list of current events.
+ *
+ * @since  1.0.0
+ */
+class HtmlView extends BaseHtmlView
+{
+
+}
