a8d26b915a0b2602141e090e449cdc86e7c83a16
Micha Buchholz <mail@lean-stack.rocks>
2025-12-03 11:55:57 +0100

    feat: Implement single event view.

--- components/com_cinetixx/tmpl/event/default.php
+++ components/com_cinetixx/tmpl/event/default.php
@@ -5,19 +5,40 @@
 use Weltspiegel\Component\Cinetixx\Administrator\Helper\YouTubeHelper;
 
 $event = $this->item;
-
 ?>
-<h1><?= $this->title ?></h1>
-
-<?php if (!empty($event->trailerId)): ?>
-    <div>
-        <iframe
-                width="522"
-                height="288"
-                src="<?= YouTubeHelper::generateTrailerLink($event->trailerId) ?>"
-                allowfullscreen
-                referrerpolicy="strict-origin-when-cross-origin"
-        ></iframe>
+<div class="border border-dark p-2">
+    <div class="d-flex d-sm-block gap-3 flex-column clearfix">
+            <div class="float-start align-self-center p-1 bg-dark me-3 mb-1" style="height: 15rem; width: 10.75rem">
+                <img src="<?= $event->poster ?>" alt="Filmplakat <?= $event->title ?>">
+            </div>
+            <h2 class="order-first"><?= $this->title ?></h2>
+            <div><?= $event->text ?></div>
+            <div class="mt-sm-3 fst-italic">
+                Dauer: <?= $event->duration ?>,
+                Sprache: <?= $event->languageShort ?>,
+                FSK: <?= $event->fsk ?>
+            </div>
     </div>
-<?php endif; ?>
+
+    <?php if (!empty($event->trailerId)): ?>
+    <div style="max-width: 640px;" class="mt-4 mx-auto">
+        <div style="padding-top: 56.25%; /* 16:9 */" class="overflow-hidden position-relative">
+            <iframe class="p-1 bg-dark position-absolute top-0 start-0 w-100 h-100"
+                    src="<?= YouTubeHelper::generateTrailerLink($event->trailerId) ?>"
+                    allowfullscreen
+                    referrerpolicy="strict-origin-when-cross-origin"
+            ></iframe>
+        </div>
+    </div>
+    <?php endif; ?>
+    <div class="mt-4 d-flex justify-content-around gap-2">
+        <?php foreach ($event->images as $ix => $image): ?>
+        <div>
+            <img class="p-1 bg-dark" src="<?= $image ?>" alt="Filmbild <?= ($ix + 1) . $event->title ?>">
+        </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+
+
 
